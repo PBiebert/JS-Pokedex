9217a95e485e641bffffd180a5c075f77a8fbbe8
Philipp Biebert <philipp.biebert@gmail.com>
2025-11-04 09:48:27 +0100

    feat: implement load and render from 20 pokemon and add the function to load more pokemon

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,76 @@
 "use strict";
+let currentStack = [];
+let counter = 20;
+let offset = 0;
+
+async function init() {
+  await loadPokemonList(counter, offset);
+  renderPokemonCard();
+  console.log(currentStack);
+}
+
+async function loadPokemonList(counter, offset) {
+  try {
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${counter}&offset=${offset}`);
+    const responseToJson = await response.json();
+    let fetchStack = responseToJson.results;
+
+    await loadPokemonDetails(fetchStack);
+
+    fetchStack = [];
+  } catch (error) {
+    console.log(`Fehler beim laden der PokemonListe`);
+  }
+}
+
+async function loadPokemonDetails(fetchStack) {
+  for (let i = 0; i < fetchStack.length; i++) {
+    try {
+      const response = await fetch(fetchStack[i].url);
+      const pokemonData = await response.json();
+
+      let pokemon = {};
+
+      pokemon.id = pokemonData.id;
+      pokemon.name = validateFirstLetterCapitalized(pokemonData.name);
+      pokemon.typs = pokemonData.types;
+      pokemon.img = pokemonData.sprites.other.dream_world.front_default;
+      currentStack.push(pokemon);
+    } catch (error) {
+      console.log(`Fehler beim laden der Details`);
+    }
+  }
+}
+
+function renderPokemonCard() {
+  const cartContainer = document.querySelector(".card-container");
+  cartContainer.innerHTML = "";
+
+  for (let i = 0; i < currentStack.length; i++) {
+    cartContainer.innerHTML += templatePokemonCard(i);
+    renderPokemonClass(i);
+  }
+}
+
+function renderPokemonClass(i) {
+  let currentPokemon = currentStack[i];
+  let currenElement = document.getElementById(`elemente_${currentStack[i].name}`);
+
+  for (let j = 0; j < currentPokemon.typs.length; j++) {
+    let element = validateFirstLetterCapitalized(currentPokemon.typs[j].type.name);
+    currenElement.innerHTML += templateElement(element);
+  }
+}
+
+function validateFirstLetterCapitalized(word) {
+  let capitalized = word;
+  capitalized = capitalized.charAt(0).toUpperCase() + capitalized.slice(1);
+  return capitalized;
+}
+
+async function loadMore() {
+  offset += counter;
+
+  await loadPokemonList(counter, offset);
+  renderPokemonCard();
+}
---
+++ src/js/templates.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function templatePokemonCard(i) {
+  return /*html*/ `
+    <div class="card-body ${currentStack[i].typs[0].type.name}" id="${currentStack[i].id}">
+        <div class="background-img">
+            <svg class="poke-background" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <path d="M3 12a9 9 0 1 0 18 0 9 9 0 1 0 -18 0"></path>
+                <path d="M9 12a3 3 0 1 0 6 0 3 3 0 1 0 -6 0"></path>
+                <path d="M3 12h6"></path>
+                <path d="M15 12h6"></path>
+            </svg>
+        </div>
+        <div class="data-container">
+            <h2 class="name">#${currentStack[i].id} ${currentStack[i].name}</h2>
+            <div class="elements" id="elemente_${currentStack[i].name}"></div>
+        </div>
+        <div class="poke-img">
+            <img src="${currentStack[i].img}" alt="Picture from ${currentStack[i].name}">
+        </div>
+    </div>`;
+}
+
+function templateElement(element) {
+  return `
+    <div class="element ${element.toLowerCase()}">${element} </div>
+    `;
+}
